6e663b56d22716cde794a5b45c41ca1cbda342f3
dlwjddus1112 <dlwjddus1112@konkuk.ac.kr>
2024-06-01 23:14:26 +0900

    OS

--- rwwlock.h
+++ rwwlock.h
@@ -1,59 +1,47 @@
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
 
 typedef struct _rwwlock_t {
-    sem_t readlock;
-    sem_t writelock;
-    int writers_waiting;
-    int readers;
     pthread_mutex_t lock;
+    pthread_cond_t cond;
+    int readers;
+    int waiting_writers;
+    bool writer_active;
 } rwwlock_t;
 
-void rwwlock_init(rwwlock_t *rww);
-void rwwlock_acquire_writelock(rwwlock_t *rww);
-void rwwlock_release_writelock(rwwlock_t *rww);
-void rwwlock_acquire_readlock(rwwlock_t *rww);
-void rwwlock_release_readlock(rwwlock_t *rww);
-
 void rwwlock_init(rwwlock_t *rww) {
-    sem_init(&rww->readlock, 0, 1);
-    sem_init(&rww->writelock, 0, 1);
-    rww->readers = 0;
-    rww->writers_waiting = 0;
     pthread_mutex_init(&rww->lock, NULL);
+    pthread_cond_init(&rww->cond, NULL);
+    rww->readers = 0;
+    rww->waiting_writers = 0;
+    rww->writer_active = false;
 }
 
 void rwwlock_acquire_writelock(rwwlock_t *rww) {
     pthread_mutex_lock(&rww->lock);
-    rww->writers_waiting++;
-    pthread_mutex_unlock(&rww->lock);
-
-    sem_wait(&rww->writelock);
-
-    pthread_mutex_lock(&rww->lock);
-    rww->writers_waiting--;
+    rww->waiting_writers++;
+    while (rww->writer_active || rww->readers > 0) {
+        pthread_cond_wait(&rww->cond, &rww->lock);
+    }
+    rww->writer_active = true;
+    rww->waiting_writers--;
     pthread_mutex_unlock(&rww->lock);
 }
 
 void rwwlock_release_writelock(rwwlock_t *rww) {
-    sem_post(&rww->writelock);
+    pthread_mutex_lock(&rww->lock);
+    rww->writer_active = false;
+    pthread_cond_broadcast(&rww->cond); // 깨우기 위해 broadcast 사용
+    pthread_mutex_unlock(&rww->lock);
 }
 
 void rwwlock_acquire_readlock(rwwlock_t *rww) {
     pthread_mutex_lock(&rww->lock);
-
-    // 만약 writer가 대기 중이라면 readlock을 획득할 때까지 대기
-    while (rww->writers_waiting > 0) {
-        pthread_mutex_unlock(&rww->lock);
-        sem_wait(&rww->readlock);
-        pthread_mutex_lock(&rww->lock);
+    while (rww->writer_active || rww->waiting_writers > 0) {
+        pthread_cond_wait(&rww->cond, &rww->lock);
     }
-
     rww->readers++;
-    if (rww->readers == 1) {
-        sem_wait(&rww->writelock);
-    }
-
     pthread_mutex_unlock(&rww->lock);
 }
 
@@ -61,11 +49,7 @@ void rwwlock_release_readlock(rwwlock_t *rww) {
     pthread_mutex_lock(&rww->lock);
     rww->readers--;
     if (rww->readers == 0) {
-        sem_post(&rww->writelock);
-    }
-    // writer가 대기 중인 경우 readlock을 해제하여 writer가 진행할 수 있도록 함
-    if (rww->writers_waiting > 0) {
-        sem_post(&rww->readlock);
+        pthread_cond_signal(&rww->cond); // 마지막 독자만 깨움
     }
     pthread_mutex_unlock(&rww->lock);
 }
