110de6f41f3f74548352087942d02a517cd98eb4
dlwjddus1112 <dlwjddus1112@konkuk.ac.kr>
2024-06-03 21:40:02 +0900

    OS

--- seqlock.h
+++ seqlock.h
@@ -1,43 +1,50 @@
+#include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
+
 typedef struct _seqlock_t {
-    int counter;
+    volatile int counter;
     sem_t readlock;
     sem_t writelock;
+    pthread_mutex_t lock;
 } seqlock_t;
-void seqlock_init(seqlock_t *seq);
-void seqlock_write_lock(seqlock_t *seq);
-void seqlock_write_unlock(seqlock_t *seq);
-unsigned seqlock_read_begin(seqlock_t *seq);
-unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt);
 
 void seqlock_init(seqlock_t *seq){
     seq->counter = 0;
-    sem_init(&seq->readlock,0,1);
-    sem_init(&seq->writelock,0,1);
+    sem_init(&seq->readlock, 0, 1);
+    sem_init(&seq->writelock, 0, 1);
+    pthread_mutex_init(&seq->lock, NULL);
 }
+
 void seqlock_write_lock(seqlock_t *seq){
+    
+    pthread_mutex_lock(&seq->lock);
+    seq->counter++; 
+    pthread_mutex_unlock(&seq->lock);
     sem_wait(&seq->writelock);
-    seq->counter++;
 }
+
 void seqlock_write_unlock(seqlock_t *seq){
+    pthread_mutex_lock(&seq->lock);
+    seq->counter++; 
+    pthread_mutex_unlock(&seq->lock);
     sem_post(&seq->writelock);
-    seq->counter++;
 }
-unsigned seqlock_read_begin(seqlock_t *seq){ // counter를 읽는다.
+
+unsigned seqlock_read_begin(seqlock_t *seq){
     unsigned cnt;
-    do{
-        cnt = seq->counter;
-        if(cnt % 2 == 1){
+    do {
+        cnt = seq->counter; // atomic read
+        // 홀수면 쓰기 작업 중이므로 대기
+        if (cnt & 1) {
             sem_wait(&seq->readlock);
             sem_post(&seq->readlock);
         }
-
-    } while(cnt % 2 == 1);
-}
-unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt){ // counter값 비교
-    return seq->counter != cnt;
+    } while (cnt & 1); // 홀수면 반복
+    return cnt;
 }
 
-// Writer는 critical section을 진입할 때, 나갈 때 한번씩 counter를 증가시킨다.
-// <Reader> counter 값을 읽는다. 그리고 벗어날 때 한번 더 읽어서 두 값이 같으면 중간에 writer가 진입하지 않았다는 것을 의미. 다르면 retry. 
-//counter 값이 같은데 홀수다 ? writer가 critical section에 진입한 상태에서 reader가 진입한 것.
+unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt){
+    // 카운터가 변경되었는지 확인
+    return seq->counter = cnt;
+}
